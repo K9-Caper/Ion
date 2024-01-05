d07febf128671fb1ebd88ba5c367e4e464b76651
K9-Caper <zaydsalahuddin@gmail.com>
2024-01-04 21:54:01 -0600

    Update ReminderManager.kt

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/managers/ReminderManager.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/managers/ReminderManager.kt
@@ -3,7 +3,53 @@ package net.horizonsend.ion.proxy.managers
 import net.horizonsend.ion.common.IonComponent
 import net.horizonsend.ion.proxy.PLUGIN
 import java.util.concurrent.TimeUnit
+import org.bukkit.Bukkit
+import org.bukkit.*
 import java.util.*
+
+
+class GalacticBroadcastPlugin : IonComponent() {
+
+    private val messages = listOf(
+        "Low on chetherite? Get your votes in, today! /vote",
+        "Be sure to visit Prometheus Station to be able to buy or sell up to 30,000 C worth of crops per day.",
+        "Bed not working? Just got blasted out of oblivion? Use /kit starter to build a cryopod.",
+			  "Commissions and cargo trading are great ways for new players to make money. Feeling confident? Try your hand at /bounty.",
+				"Need an item? Use /bazaar browse, although be aware: the prices are quadrupled if you're buying remotely!",
+        "24 siegeable stations exist in space. Only nations can siege stations, and owning between 1-6 stations reaps benefits for all nation members.",
+        "Just starting out? Try visiting the planets of Luxiterna or Aret. Both have resources valuable for new players.",
+				"Level 60 is currently the highest level required to unlock the biggest avaialable ship class: Destroyers / Heavy Friegthers (12,000 blocks).",
+				"Want to pick a fight with NPCs? It's recommended you take a gunship, at least.",
+				"Looking for a settlement or nation to join? Be sure to ask in global chat using /g. There are always players recruiting.",
+			  "Setting up machinery? Solar power and Gas generators are clean and reliable. Do /wiki to learn more.",
+				"Don't forget to enable your ship lock when parking in territory that's not your own. The lock is toggleable in the ship computer (jukebox).",
+				"Use /starship when piloting a ship to get a diagnostic overview of your vessel. This is quite useful for shipbuilding and repairs.",
+				"Unsure who someone is? Use /pinfo [player]."
+
+        // expand list here
+    )
+
+    private var currentIndex = 0
+
+    override fun onEnable() {
+        scheduleGalacticBroadcast()
+    }
+
+    private fun scheduleGalacticBroadcast() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, {
+            broadcastNextMessage()
+        }, 0, 20 * 60 * 30) // 20 ticks per second, 60 seconds per minute, 30 minutes per broadcast
+    }
+
+    private fun broadcastNextMessage() {
+        val nextMessage = messages[currentIndex]
+        Bukkit.broadcastMessage(ChatColor.GREEN.toString() + "[Galactic Broadcast] $nextMessage")
+
+        currentIndex = (currentIndex + 1) % messages.size
+// broadcastnextmessage might need fine tuning
+    }
+}
+
 
 // below is glutin code
 //
